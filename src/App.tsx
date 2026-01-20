3aa34623d72ca1a8beb2e02f5d07fcad0a70b591
Victoria-Reiss <v.oliveirareiss@gmail.com>
2026-01-20 10:08:21 -0300

    redirect url

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Formulario from "./pages/Formulario/Formulario";
+// import Contabilidade from "./pages/Contabilidade"
 
 const queryClient = new QueryClient();
 
@@ -15,9 +16,10 @@ const App = () => (
       <Toaster />
       <Sonner />
       <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<NotFound />} />
+        {/* <Route path="/" element={<Index />} /> */}
+        <Route path="/contabilidade" element={<Index />} />
         <Route path="/formulario" element={<Formulario />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </TooltipProvider>
   </QueryClientProvider>
---
+++ src/pages/Contabilidade.tsx
@@ -0,0 +1,8 @@
+export default function Accounting() {
+  return (
+    <main>
+      <h1>Accounting Landing Page</h1>
+      <p>This is the accounting LP - teste vic.</p>
+    </main>
+  );
+}
